c40b249444d8d5746ab80887f8000cfce799bf61
Taniya Anjalie <anjalietaniya@gmail.com>
2023-10-20 18:07:59 +0530

    database connection

--- backend/Server.js
+++ backend/Server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Define port
 
+// Require the database connection
+const connectDB = require('./Database'); 
+
+// Connect to the database
+connectDB();
+
 // Define routes
 app.get('/', (req, res) => {
     res.send('Hello, World!');
